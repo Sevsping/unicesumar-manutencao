df4130af834c11740ad3a823522a0d5c1153666c
Sevsping <silviojuniorferias@gmail.com>
2026-05-15 21:12:46 -0300

    test: ajusta teste do bug 04 para evitar falso positivo

--- library-maintenance-lab/src/test/java/BookManagerTest.java
+++ library-maintenance-lab/src/test/java/BookManagerTest.java
@@ -2,7 +2,7 @@ import org.junit.Test;
 
 public class BookManagerTest {
 
-    @Test(expected = RuntimeException.class)
+    @Test(expected = IllegalStateException.class)
     public void deveFalharQuandoNaoExistemLivros() {
         BookManager manager = new BookManager();
 

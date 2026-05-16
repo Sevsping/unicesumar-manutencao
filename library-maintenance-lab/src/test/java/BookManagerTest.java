f3d4a31b1233c5ad57843ec57d639081a6b5a986
Sevsping <silviojuniorferias@gmail.com>
2026-05-15 22:23:01 -0300

    test: ajusta testes do BookManager para isolamento e valida fluxo com sucesso e log INFO
    Clear the LegacyDatabase books before testing book listing.

--- library-maintenance-lab/src/test/java/BookManagerTest.java
+++ library-maintenance-lab/src/test/java/BookManagerTest.java
@@ -5,16 +5,20 @@ public class BookManagerTest {
     @Test(expected = IllegalStateException.class)
     public void deveFalharQuandoNaoExistemLivros() {
         BookManager manager = new BookManager();
+        LegacyDatabase.getBooks().clear();
 
+        BookManager manager = new BookManager();
         manager.listBooksSimple();
-    }
+    
 }
 
 @Test
 public void deveListarLivrosComSucesso() {
-    BookManager manager = new BookManager();
+     LegacyDatabase.getBooks().clear();
 
+    BookManager manager = new BookManager();
     manager.registerBook("Livro Teste", "Autor Teste", 2024, "GERAL", 1, 1, "A1", "ISBN-001");
 
     manager.listBooksSimple();
+    }
 }
